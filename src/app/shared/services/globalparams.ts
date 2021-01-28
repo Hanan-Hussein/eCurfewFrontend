6dd3a5b3829da02a6d44e7e9170d6d294c116cdf
1sarah <mbuyaeugenis@gmail.com>
2021-01-28 13:19:42 +0300

    filters

--- src/app/shared/services/globalparams.ts
+++ src/app/shared/services/globalparams.ts
@@ -7,8 +7,8 @@ export class GlobalParams {
 
   // baseUrl = 'http://localhost:8069/api/v1/';
   baseUrl = 'http://41.215.130.247:6160/';
-  // baseUrl = 'http://localhost:8080/';
-  // baseUrl = 'http://192.168.1.128:6160/';
+   //baseUrl = 'http://localhost:8080/';
+   //baseUrl = 'http://192.168.1.128:6160/';
 
   emailPattern = '([A-Za-z0-9_\\-\\.]+)@[A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*(\\.[A-Za-z]{2,3})$';
   phonePattern = '^[2][5][4][0-9]{9}$';
